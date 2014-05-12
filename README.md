bc0cb4bce8d4a43814ad1eedf12e73ba48d80e01
Sebastian Sogamoso <sebasoga@gmail.com>
2014-05-12 04:43:19 -0500

    Welcome zsh

--- README.md
+++ README.md
@@ -9,8 +9,12 @@ Clone onto your machine:
 Install:
 
     cd dotfiles
-    sh install.sh
+    sh install-dotfiles
 
-This will create symlinks to config files in your home directory overwriting the existing ones.
+This will:
+* Create symlinks to config files in your home directory overwriting the existing ones.
+* Install Vim plugins
+* Install Homebrew and a few useful packages
+* Install oh-my-zsh and enable a few plugins
 
-You can safely run `sh install.sh` multiple times to update.
+You can safely run `sh install-dotfiles` multiple times to update.
---
+++ zsh/sebasoga.plugin.zsh
@@ -0,0 +1,29 @@
+# Functions
+function grepp(){
+  grep -Rni --exclude=".//db/data*" --exclude=".//tmp*" --exclude=".//vendor*" --exclude=".//coverage*" --exclude=".//log*" --exclude=".//tag*" --exclude=".//spec*/cassett*" --exclude=".//public/*.js*" --exclude=".//.*" "$*" ./
+}
+
+function findd(){
+  find . -ipath "*$**"
+}
+
+function reload(){
+  source ~/.zshrc
+}
+
+# Editor
+if [[ "$EDITOR" == "" ]] ; then
+  export EDITOR="vim"
+fi
+
+# Aliases
+alias code="cd $HOME/Code"
+alias dotfiles="cd $HOME/Code/dotfiles"
+
+alias gadd='git add --all && git status'
+alias glog='git log'
+
+alias rc_test='RAILS_ENV=test bundle exec rails console --sandbox'
+alias db_migrate='bundle exec rake db:migrate db:test:prepare'
+alias db_setup='bundle exec rake db:setup db:test:prepare'
+alias db_redo='bundle exec rake db:migrate:redo'
