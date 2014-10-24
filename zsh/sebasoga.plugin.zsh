e62696fcf0acac0661e0cf6be5a0f134484381f3
Sebastian Sogamoso <sebasoga@gmail.com>
2014-10-23 22:07:46 -0500

    Update custom zsh plugin

--- zsh/sebasoga.plugin.zsh
+++ zsh/sebasoga.plugin.zsh
@@ -21,14 +21,14 @@ if [[ "$EDITOR" == "" ]] ; then
 fi
 
 # Aliases
-alias code="cd $HOME/Code"
-alias dotfiles="cd $HOME/Code/dotfiles"
-alias idot="cd $HOME/Code/dotfiles && sh install-dotfiles && source ~/.zshrc"
+alias code="cd $HOME/src"
+alias dotfiles="cd $HOME/src/dotfiles"
+alias idot="cd $HOME/src/dotfiles && sh install-dotfiles && source ~/.zshrc"
 
 alias gadd='git add --all && git status'
 alias glog='git log'
 
 alias rc_test='RAILS_ENV=test bundle exec rails console --sandbox'
-alias db_migrate='bundle exec rake db:migrate db:rollback && bundle exec rake db:migrate db:test:prepare'
-alias db_setup='bundle exec rake db:setup db:test:prepare'
+alias db_migrate='bundle exec rake db:migrate db:rollback && bundle exec rake db:migrate'
+alias db_setup='bundle exec rake db:setup'
 alias db_redo='bundle exec rake db:migrate:redo'
