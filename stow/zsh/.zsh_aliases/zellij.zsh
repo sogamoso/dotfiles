9ca1d4182d43f11215624722715530e733e4b504
Sebastian Sogamoso <sebastian@sogamo.so>
2026-02-12 20:03:59 -0500

    Add zt function to rename zellij tabs

--- stow/zsh/.zsh_aliases/zellij.zsh
+++ stow/zsh/.zsh_aliases/zellij.zsh
@@ -1,1 +1,9 @@
 alias zdev="zellij --layout dev attach -c dev"
+alias tab="zt"
+
+zt() {
+  local name="${1:-${PWD:t}}"
+  command -v zellij >/dev/null 2>&1 || return 0
+  [[ -n "$ZELLIJ" ]] || return 0
+  zellij action rename-tab "$name"
+}
