b5b02426b6cc40a0080570a9866acd4f40a4d626
Sebastian Sogamoso <sebastian@sogamo.so>
2026-02-24 22:53:53 -0500

    Replace hotkeys PWA with Hammerspoon overlay

--- stow/macos/.config/raycast/script-commands/show-hotkeys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# @raycast.schemaVersion 1
-# @raycast.title Show Hotkeys
-# @raycast.mode silent
-# @raycast.packageName Utilities
-# @raycast.icon ⌨️
-
-open "$HOME/Applications/Chrome Apps.localized/Hotkeys.app"
