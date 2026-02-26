3c4469962124162ab40f32e8c7bd0c8f897de1ff
Sebastian Sogamoso <sebastian@sogamo.so>
2026-02-26 14:56:44 -0500

    Reload Hammerspoon in reload shell alias

--- stow/macos/.config/zsh/aliases/shell.zsh
+++ stow/macos/.config/zsh/aliases/shell.zsh
@@ -3,5 +3,8 @@
 reload() {
   (cd "$HOME/Code/dotfiles/stow" && stow --target "$HOME" --restow */) || return
   source "$HOME/.zshrc" || return
+  if [[ -d "/Applications/Hammerspoon.app" ]]; then
+    open "hammerspoon://reload" >/dev/null 2>&1 || return
+  fi
   clear
 }
