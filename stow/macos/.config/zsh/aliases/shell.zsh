5f749ca0660bcbed0762e7924606c950b8a98415
Sebastian Sogamoso <sebastian@sogamo.so>
2026-02-26 11:50:31 -0500

    Only clear on successful reload

--- stow/macos/.config/zsh/aliases/shell.zsh
+++ stow/macos/.config/zsh/aliases/shell.zsh
@@ -1,6 +1,7 @@
 # General shell utilities
 
 reload() {
-  (cd "$HOME/Code/dotfiles/stow" && stow --target "$HOME" --restow */)
-  source "$HOME/.zshrc"
+  (cd "$HOME/Code/dotfiles/stow" && stow --target "$HOME" --restow */) || return
+  source "$HOME/.zshrc" || return
+  clear
 }
