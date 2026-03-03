2dfe318cb9db098ff447b0f57a67e620b4a3840c
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-03 12:04:52 -0500

    Rename ghprc alias to ghprck to fix conflict with ghprc function

--- stow/zsh/.config/zsh/aliases/gh.zsh
+++ stow/zsh/.config/zsh/aliases/gh.zsh
@@ -1,6 +1,6 @@
 alias ghpr='gh pr create --fill'
 alias ghprw='gh pr view --web'
-alias ghprc='gh pr checks'
+alias ghprck='gh pr checks'
 alias ghprr='gh pr review'
 alias ghprs='gh pr status'
 alias ghrepo='gh repo view --web' # Open current repo in browser
