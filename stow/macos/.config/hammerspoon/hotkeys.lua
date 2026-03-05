70b26016793ea124ff71438b274960a7148136e6
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-05 16:54:32 -0500

    Move window focus bindings from init.lua to hotkeys.lua

--- stow/macos/.config/hammerspoon/hotkeys.lua
+++ stow/macos/.config/hammerspoon/hotkeys.lua
@@ -1,4 +1,11 @@
 local hotMods = { "cmd", "ctrl" }
+
+-- Window focus by direction
+hs.hotkey.bind(hotMods, "right", function() hs.window.focusedWindow():focusWindowEast() end)
+hs.hotkey.bind(hotMods, "left",  function() hs.window.focusedWindow():focusWindowWest() end)
+hs.hotkey.bind(hotMods, "up",    function() hs.window.focusedWindow():focusWindowNorth() end)
+hs.hotkey.bind(hotMods, "down",  function() hs.window.focusedWindow():focusWindowSouth() end)
+
 local cheatsheet = nil
 local keyTap = nil
 
--- stow/macos/.config/hammerspoon/init.lua
+++ stow/macos/.config/hammerspoon/init.lua
@@ -1,25 +1,10 @@
-local mods = { "cmd", "ctrl" }
-
-hs.hotkey.bind(mods, "right", function()
-	hs.window.focusedWindow():focusWindowEast()
-end)
-
-hs.hotkey.bind(mods, "left", function()
-	hs.window.focusedWindow():focusWindowWest()
-end)
-
-hs.hotkey.bind(mods, "up", function()
-	hs.window.focusedWindow():focusWindowNorth()
-end)
-
-hs.hotkey.bind(mods, "down", function()
-	hs.window.focusedWindow():focusWindowSouth()
-end)
 require("hotkeys")
 require("aerospace_follow")
 
-hs.screen.watcher.new(function()
-  hs.timer.doAfter(2, function()
-    hs.execute("/opt/homebrew/bin/sketchybar --reload")
-  end)
-end):start()
+hs.screen.watcher
+	.new(function()
+		hs.timer.doAfter(2, function()
+			hs.execute("/opt/homebrew/bin/sketchybar --reload")
+		end)
+	end)
+	:start()
