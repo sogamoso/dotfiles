341beac5efdd9514e7d26059f8324d8fb3d956cf
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-05 23:58:34 -0500

    Separate cpu and memory icons with load-based color, no labels

--- stow/macos/.config/sketchybar/plugins/memory.sh
+++ stow/macos/.config/sketchybar/plugins/memory.sh
@@ -2,11 +2,18 @@
 
 PAGE_SIZE=$(vm_stat | awk '/page size of/{print $8}')
 TOTAL=$(sysctl -n hw.memsize)
-USED=$(vm_stat | awk -v ps="$PAGE_SIZE" '
+MEM=$(vm_stat | awk -v ps="$PAGE_SIZE" -v total="$TOTAL" '
   /Pages active/   { active = $3 }
   /Pages wired/    { wired  = $4 }
-  END { gsub(/\./, "", active); gsub(/\./, "", wired); printf "%.0f", (active + wired) * ps }
+  END { gsub(/\./, "", active); gsub(/\./, "", wired); printf "%.0f", (active + wired) * ps / total * 100 }
 ')
-PCT=$(awk "BEGIN {printf \"%.0f\", $USED/$TOTAL*100}")
 
-sketchybar --set "$NAME" icon="󰍛" label="${PCT}%"
+if [ "$MEM" -gt 80 ]; then
+  COLOR=0xfff7768e
+elif [ "$MEM" -gt 50 ]; then
+  COLOR=0xffe0af68
+else
+  COLOR=0xffa9b1d6
+fi
+
+sketchybar --set "$NAME" icon="󰍛" icon.color=$COLOR label.drawing=off
