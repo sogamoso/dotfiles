8261f6a89c41a10270d76118b7ccef922e6639ad
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-06 10:31:15 -0500

    Revert Activity Monitor tab switching, just open the app

--- stow/macos/.config/sketchybar/plugins/memory_click.sh
+++ stow/macos/.config/sketchybar/plugins/memory_click.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-osascript <<'EOF'
-tell application "Activity Monitor" to activate
-tell application "System Events"
-  tell process "Activity Monitor"
-    repeat until (exists menu bar 1)
-      delay 0.1
-    end repeat
-    click menu item "Memory" of menu "View" of menu bar 1
-  end tell
-end tell
-EOF
+open -a "Activity Monitor"
