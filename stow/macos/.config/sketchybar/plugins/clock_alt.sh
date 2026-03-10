eb60237a35294ed0c40dd0f12be321274a494c5a
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-10 18:33:20 -0500

    Revert "Remove external display detection from sketchybar"
    This reverts commit b5d991d72a16485a16f561860a1c0ad0f8e23a61.

--- stow/macos/.config/sketchybar/plugins/clock_alt.sh
+++ stow/macos/.config/sketchybar/plugins/clock_alt.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 normal_label() {
-  date '+%b %-d | %H:%M | %A'
+  HAS_EXTERNAL=$(cat /tmp/sketchybar_has_external 2>/dev/null || echo 0)
+  if [ "$HAS_EXTERNAL" -eq 1 ]; then
+    date '+%b %-d | %H:%M | %A'
+  else
+    date '+%b %-d %H:%M'
+  fi
 }
 
 STATE=$(cat /tmp/sketchybar_clock_alt 2>/dev/null || echo "normal")
