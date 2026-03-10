c496a76cc3fcba30af9e499f6e8f6df27ec1b99c
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-10 11:23:51 -0500

    Install Homebrew in brew.sh if not already in PATH

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -3,7 +3,15 @@ set -euo pipefail
 
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if ! command -v brew &>/dev/null; then
+  if [[ -x /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    echo "==> Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+fi
 
 echo -e "\n==> Installing Homebrew packages..."
 
