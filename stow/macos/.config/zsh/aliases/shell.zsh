bedc7593c43bcb36ace1de2aa470ab88616d906d
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-17 16:15:36 -0500

    Fix reload: remove early returns that prevented clear from running

--- stow/macos/.config/zsh/aliases/shell.zsh
+++ stow/macos/.config/zsh/aliases/shell.zsh
@@ -2,16 +2,14 @@
 
 reload() {
   {
-    bash "$HOME/Code/dotfiles/install/dotfiles/all.sh" || return
-    bash "$HOME/Code/dotfiles/install/macos/dotfiles.sh" || return
-    source "$HOME/.zshrc" || return
-    if command -v sketchybar &>/dev/null; then
-      sketchybar --reload
-    fi
-    if command -v aerospace &>/dev/null; then
+    bash "$HOME/Code/dotfiles/install/dotfiles/all.sh"
+    bash "$HOME/Code/dotfiles/install/macos/dotfiles.sh"
+    source "$HOME/.zshrc"
+    command -v sketchybar &>/dev/null && sketchybar --reload
+    command -v aerospace &>/dev/null && {
       "$HOME/.config/sketchybar/plugins/sync_aerospace_gap.sh"
       aerospace reload-config
-    fi
+    }
     fc -R
   } &>/dev/null
   clear
