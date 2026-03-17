2ca5dbf770d8f29e1ed9ce5174e07276a2d869df
Sebastian Sogamoso <sebastian@sogamo.so>
2026-03-17 09:20:53 -0500

    Add completion messages to macOS install scripts

--- install/macos/preferences.sh
+++ install/macos/preferences.sh
@@ -41,3 +41,5 @@ defaults write com.apple.dock orientation -string "right"
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock tilesize -int 43
 killall Dock
+
+echo -e "✓ macOS preferences set"
